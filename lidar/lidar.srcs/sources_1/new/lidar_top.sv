840576080652aefc0682fbbe2e5571b726a6ac44
KJW <kijw0003@gmail.com>
2026-04-06 10:21:58 +0900

    폴더 구조 정리

---
+++ lidar/lidar.srcs/sources_1/new/lidar_top.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/04/06 10:21:18
+// Design Name: 
+// Module Name: lidar_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module lidar_top(
+
+    );
+endmodule
