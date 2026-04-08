96d6cd65210a2ab28dc99ca3a765cafb54393d86
KJW <kijw0003@gmail.com>
2026-04-08 10:34:01 +0900

    reafctor: 타이밍 제약 충족을 위한 [angle_calc.sv] 구조 개선

--- lidar/lidar.srcs/sources_1/new/angle_calc.sv
+++ lidar/lidar.srcs/sources_1/new/angle_calc.sv
@@ -24,6 +24,7 @@ module angle_calc (
                              raw_diff :
                              (9'd360 - angle_fsa + angle_lsa);
 
+    logic [8:0] angle_diff_reg;
     // --------------------------------------------------
     // LUT 출력
     // --------------------------------------------------
@@ -45,7 +46,6 @@ module angle_calc (
     // --------------------------------------------------
     // 내부 레지스터
     // --------------------------------------------------
-    logic [14:0] fsa_q6;
     logic [14:0] step_q6;
     logic [14:0] accum_q6;
 
@@ -55,41 +55,58 @@ module angle_calc (
     // 곱셈 파이프라인용
     logic [30:0] mult_tmp;   // (diff<<6)=15bit × recip(16bit) → 31bit 필요하지만 최적화 가능
 
+    logic fsa_lsa_valid_1;
+    logic [8:0] lsn_reg;
     // --------------------------------------------------
     // 메인 로직
     // --------------------------------------------------
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            fsa_q6      <= '0;
-            step_q6     <= '0;
-            accum_q6    <= '0;
-            lat_lsn1    <= '0;
-            lat_fsa     <= '0;
-            angle_deg   <= '0;
-            angle_valid <= '0;
-            mult_tmp    <= '0;
+            step_q6        <= '0;
+            accum_q6       <= '0;
+            lat_lsn1       <= '0;
+            lat_fsa        <= '0;
+            angle_deg      <= '0;
+            angle_valid    <= '0;
+            mult_tmp       <= '0;
+            angle_diff_reg <= 0;
+            lsn_reg        <= 0;
         end else begin
             angle_valid <= 1'b0;
+
 
             // ------------------------------------------
             // STEP 계산 (division 제거!)
             // ------------------------------------------
             if (fsa_lsa_valid) begin
-                lat_fsa  <= angle_fsa;
-                lat_lsn1 <= (lsn == 8'd1);
+                lat_fsa <= angle_fsa;
+                lsn_reg <= lsn;
+                angle_diff_reg <= angle_diff;
 
-                // fsa_q6 = angle_fsa * 64
-                fsa_q6   <= {angle_fsa, 6'b0};
+                fsa_lsa_valid_1 <= 1;
+
 
                 if (lsn == 8'd1) begin
-                    step_q6 <= 0;
+                    lat_lsn1 <= 1;
                 end else begin
                     // 1-cycle pipeline (timing 안정)
-                    mult_tmp <= ({angle_diff, 6'b0}) * recip_q16;
-                    step_q6  <= mult_tmp[30:16];  // >>16
+                    lat_lsn1 <= 0;
                 end
 
                 accum_q6 <= {angle_fsa, 6'b0};
+            end
+
+            if (fsa_lsa_valid_1) begin
+
+                fsa_lsa_valid_1 <= 0;
+
+                if (lsn_reg == 8'd1) begin
+                    step_q6 <= 0;
+                end else begin
+                    // 1-cycle pipeline (timing 안정)
+                    mult_tmp <= {angle_diff_reg, 6'b0} / (lsn_reg - 1);
+                    step_q6  <= mult_tmp[30:16];  // >>16
+                end
             end
 
             // ------------------------------------------
@@ -109,3 +126,5 @@ module angle_calc (
     end
 
 endmodule
+
+
