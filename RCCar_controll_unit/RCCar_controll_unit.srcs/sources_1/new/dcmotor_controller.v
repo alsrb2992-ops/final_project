5a7e2ab604131cc0b7f7152d4250551b92e4ae74
KJW <kijw0003@gmail.com>
2026-05-14 11:04:18 +0900

    test: 현장에 맞게 dc 속도 올림

--- RCCar_controll_unit/RCCar_controll_unit.srcs/sources_1/new/dcmotor_controller.v
+++ RCCar_controll_unit/RCCar_controll_unit.srcs/sources_1/new/dcmotor_controller.v
@@ -19,7 +19,7 @@ module dcmotor_controller #(
 
     localparam pwm_period = 5000;  // Hz  
     localparam forward_back_ms = 88;  // %     
-    localparam turn_ms = 90;  // %     
+    localparam turn_ms = 92;  // %     
     localparam pwm_period_cnt = CLK_FREQ / pwm_period;
     localparam forward_back_cnt = (pwm_period_cnt * forward_back_ms) / 100;
     localparam turn_cnt = (pwm_period_cnt * turn_ms) / 100;
